317ff0a7d93e072ec4dea760d4a471efc589dbd5
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2023-03-01 01:58:42 +0600

    responsive issue fixed

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className='bg-[#0F1312]'>
-            <div className='container mx-auto w-11/12 lg:w-full flex justify-between py-24'>
-                <div>
-                    <div className="flex items-center gap-4">
+            <div className='container mx-auto w-11/12 lg:w-full flex flex-col lg:flex-row justify-between py-24'>
+                <div className='pb-10 lg:pb-0'>
+                    <div className="flex items-center gap-4 justify-center lg:justify-start">
                         <picture>
                             <img src='/img/logo.png' alt='hero' />
                         </picture>
@@ -14,13 +14,13 @@ export default function Footer() {
                         </h3>
                     </div>
 
-                    <p className='text-gray-200 py-5'>
+                    <p className='text-gray-200 py-5 text-center lg:text-left'>
                         The premier Solana pre-sale NFT platform.
                     </p>
                 </div>
 
                 {/* footer menus 3 column */}
-                <div className="flex items-center gap-24">
+                <div className="flex flex-col lg:flex-row items-center gap-24">
                     <div className="flex flex-col gap-5">
                         <h3 className='text-white text-lg font-semibold'>
                             Payment
