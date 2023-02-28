ad3edbb6d83f52d824fa2e79e23c6ec8770daf7a
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2023-02-28 12:59:37 +0600

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+
+import Header from '../components/shared/Header'
+import Hero from '../components/LandingPageComponents/Hero'
+import ActivePreSale from '../components/LandingPageComponents/ActivePreSale'
+import UpcomingPreSale from '../components/LandingPageComponents/UpcomingPreSale'
+import Footer from '../components/shared/Footer'
 
 const Home: NextPage = () => {
   return (
@@ -10,10 +15,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
-
-
-
+      <Header />
+      <Hero />
+      <ActivePreSale />
+      <UpcomingPreSale />
+      <Footer />
 
     </div>
   )
